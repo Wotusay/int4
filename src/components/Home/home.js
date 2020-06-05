914796a92f58b1c095399ada0ce1c45fef54a4df
wotusay <wout.salembier@hotmail.com>
2020-06-05 13:10:46 +0200

    added things

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -5,26 +5,30 @@ import Picture from "../Picture/picture";
 import styles from "./home.module.css";
 import { ROUTES } from "../../consts";
 import { Link} from 'react-router-dom';
-
+import Draggable from 'react-draggable';
 
 
 const Home = () => {    
 
-    
 
     const store = useStore();
 
+
+
+    
     return useObserver(() => (
-        <section >
+
+            <section >
             <h1 className={styles.title}>Photobook</h1>
             <ul className={styles.grid}  > 
                 {store.pictures.map(picture => (
-                
+                    <Draggable> 
                     <li key={picture.id}>
+                    
                     <Link to={ROUTES.pictureDetail.to + picture.id}>
-                   <Picture  picture={picture} />  
+                  <Picture  picture={picture} /> 
                    </Link>   
-                    </li>
+                    </li></Draggable> 
                 )
                     
                 )}
@@ -34,7 +38,11 @@ const Home = () => {
                 </li>
             </ul>
         </section>
-    ))
+        
+
+    )
+
+    )
 
 }
 

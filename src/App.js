138c9554433a34f7f59aab2c1d6ed50a7c6947fc
Your Name HereEwout <ewout.verhamme@student.howest.be>
2020-06-14 15:06:02 +0200

    added HTML & CSS login

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React from 'react';
+import React from "react";
 import styles from "./App.module.css";
-import { Switch, Route } from 'react-router-dom';
-import Picturebook from './components/Picturebook/picturebook'
-import { ROUTES } from './consts';
-import PicturesDetail from './components/PicturesDetail/PicturesDetail.js';
-import Room from './components/Room/room';
-import Header from './components/Header/header';
-import Login from './components/Login/login';
-
-
+import { Switch, Route } from "react-router-dom";
+import Picturebook from "./components/Picturebook/picturebook";
+import { ROUTES } from "./consts";
+import PicturesDetail from "./components/PicturesDetail/PicturesDetail.js";
+import Room from "./components/Room/room";
+import Header from "./components/Header/header";
+import Login from "./components/Login/login";
 
 function App() {
   return (
     <>
-    <Header/>
-    <div className={styles.container}>
-      <Switch>
-      <Route exact path={ROUTES.home}>
-          <Login/>
-        </Route>
-        <Route exact path={ROUTES.room}>
-          <Room/>
-        </Route>
+      <Header />
+      <div className={styles.container}>
+        <Switch>
+          <Route exact path={ROUTES.home}>
+            <Login />
+          </Route>
+          <Route exact path={ROUTES.room}>
+            <Room />
+          </Route>
 
-        <Route exact path={ROUTES.pictureBook}>
-          <Picturebook/>
-        </Route>
-        <Route  path={ROUTES.pictureDetail.path}>
-          <PicturesDetail/>
-        </Route>
-      </Switch>
-    </div>
+          <Route exact path={ROUTES.pictureBook}>
+            <Picturebook />
+          </Route>
+          <Route path={ROUTES.pictureDetail.path}>
+            <PicturesDetail />
+          </Route>
+        </Switch>
+      </div>
     </>
   );
 }
--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./login.module.css";
+import { ROUTES } from "../../consts";
+import {Redirect } from "react-router-dom";
 
 const Login = () => {
+  const [code, setCode] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (code === "3403 - XPD2  - SPA1 - DPE2") {
+      console.log("logged in");
+      <Redirect to={ROUTES.home} />
+    } else {
+      console.log("error");
+    }
+  };
+
   return (
     <>
-      <div className={styles.test}>
-        <p>geen header gebruiken in de components dit staat in app.js en geen container gebruiken dit staat ook in app.js</p>
+      <div className={styles.login}>
+        <p className={styles.loginIntro}>Vul uw code hier in.</p>
+        <form>
+          <input
+            value={code}
+            type="text"
+            className={styles.loginInput}
+            placeholder="3403 - XPD2  - SPA1 - DPE2"
+            onChange={(e) => setCode(e.currentTarget.value)}
+          ></input>
+          <div className={styles.loginButtonWrapper}>
+            <button className={styles.loginButtonWhere}>
+              Waar vindt ik de code?
+            </button>
+            <button onClick={handleLogin} className={styles.loginButton}>
+              Inloggen
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );

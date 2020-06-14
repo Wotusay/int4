c3904f8e7a4934d4729873f1a056765c07e8cb95
Your Name HereEwout <ewout.verhamme@student.howest.be>
2020-06-14 13:27:47 +0200

    added login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,1 +1,15 @@
-.
+import React from "react";
+import styles from "./login.module.css";
+import Header from "../Header/header";
+
+const Login = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <Header />
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- src/components/Room/room.js
+++ src/components/Room/room.js
@@ -5,37 +5,28 @@ import Indicator from "../Indicator/indicator";
 
 const Room = () => {
 
-  
-    return (
-        <>
-            <div className={styles.pic}>
-            <Buttons />
+  return (
+    <>
+      <div className={styles.pic}>
+        <Buttons />
 
-            <picture>       
-                <source 
-            media="(max-width: 320px)" srcset="assets/room1-mobile.jpg" 
-          />
-                      <source 
-            media="(max-width: 767px)" srcset="assets/room1-tablet.jpg" 
-          />
-          <source 
-            media="(max-width: 1440px)" srcset="assets/room1.jpg" 
+        <picture>
+          <source media="(max-width: 320px)" srcset="assets/room1-mobile.jpg" />
+          <source media="(max-width: 767px)" srcset="assets/room1-tablet.jpg" />
+          <source media="(max-width: 1440px)" srcset="assets/room1.jpg" />
+
+          <source
+            media="(max-width: 1920px)"
+            srcset="assets/room1-desktop.jpg"
           />
 
-            <source 
-            media="(max-width: 1920px)" srcset="assets/room1-desktop.jpg" 
-          />
-   
-          <img 
-            src="assets/room1.jpg" 
-            alt="Room" 
-            style={{width: 'auto'}} 
-          />
+          <img src="assets/room1.jpg" alt="Room" style={{ width: "auto" }} />
         </picture>
-            </div>
-            <Indicator />
-        </>
-    )
-}
+      </div>
+      <Indicator />
 
-export default Room;  
+    </>
+  );
+};
+
+export default Room;

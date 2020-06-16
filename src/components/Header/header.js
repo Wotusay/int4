124953b086f76c4463ad46e4eb2139145023a099
wotusay <wout.salembier@hotmail.com>
2020-06-16 12:29:18 +0200

    umm some didnt check the index.css all the rem sizes are wrong

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./header.module.css";
 import { Link, useHistory } from "react-router-dom";
-import { ROUTES } from "../../consts";
+import { ROUTES, mediaQueries } from "../../consts";
 import { useStores } from "../../hooks/index";
 import { useObserver } from "mobx-react-lite";
+import MediaQuery from "react-responsive";
 
 const Header = () => {
   const { uiStore } = useStores();
@@ -22,7 +23,12 @@ const Header = () => {
       <div className={styles.headerWrapper}>
         <div className={styles.header}>
           <Link to={ROUTES.home} style={{ textDecoration: "none" }}>
+          <MediaQuery maxDeviceWidth={mediaQueries.desktop} minDeviceWidth={mediaQueries.tablet+1}>
             <h1 className={styles.logo}>HoneyHome</h1>
+            </MediaQuery>
+            <MediaQuery maxDeviceWidth={mediaQueries.tablet}>
+            <h1 className={styles.logo}>H</h1>
+            </MediaQuery>
           </Link>
           <div className={styles.buttons}>
             <Link style={{ textDecoration: "none" }}>

fd0761f49ed2ac8a636ba8aba5781e60228915b2
wotusay <wout.salembier@hotmail.com>
2020-06-17 19:06:38 +0200

    added hover one

--- src/components/Boxes/boxes.js
+++ src/components/Boxes/boxes.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { useObserver } from "mobx-react-lite";
 import styles from "./boxes.module.css";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams, useHistory,Link } from "react-router-dom";
 import AdventureBox from "../AdventBox/adventbox";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { ROUTES } from "../../consts";
@@ -24,7 +24,9 @@ const Boxes = () => {
 
     return useObserver(() => (
         <>
-        <div className={styles.wrapper}>
+        {id === 'adventure-box' ? <> <img className={styles.leave1} alt="prop1" src="/assets/adventurebox/leaves-bottom.png" />  <img className={styles.leave2}  alt="prop2" src="/assets/adventurebox/leaves-top.png" />  </>: id === 'romantic-box' ? 'Amura box  €40' : id === 'tropic-box' ? 'Tropicae box   €40' : <Redirect to={ROUTES.boxes.to + '/adventure-box'}  /> }
+
+        <div className={id === 'adventure-box' ? styles.adventure_wrapper : id === 'romantic-box' ? styles.romantic_wrapper : id === 'tropic-box' ? styles.tropic_wrapper : styles.wrapper}>
         <button onClick={e => handlePrevious(e)}  className={styles.button_prev}>
         <svg width="30" height="54" viewBox="0 0 30 54" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M28 52L3 25.0263L28 2" stroke="white" strokeWidth="4"/>
@@ -36,9 +38,22 @@ const Boxes = () => {
             <svg width="30" height="54" viewBox="0 0 30 54" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2 2L27 28.9737L2 52" stroke="white" strokeWidth="4"/>
             </svg>
-        </button></div>
+        </button>   
+        <div className={styles.info_wrapper}>
+            <div className={styles.pos}>  
+            <p className={styles.title}>
+            {id === 'adventure-box' ? 'Advontura box   €40' : id === 'romantic-box' ? 'Amura box  €40' : id === 'tropic-box' ? 'Tropicae box   €40' : <Redirect to={ROUTES.boxes.to + '/adventure-box'}  /> }
+            </p>
+            <div className={styles.buttons_wrapper}>
+                <Link className={styles.button_main}>Koop Box</Link>
+                <Link className={styles.button_sec}>Doe de test</Link>
+            </div>
+            <p className={styles.info}>Hover over the items voor meer info</p>
+        </div> </div>    
+   </div>
 
-        <div></div>
+
+
         </>
     ))
 

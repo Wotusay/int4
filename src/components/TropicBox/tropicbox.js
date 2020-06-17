4d35eb4abd4f1b06c4a22ecf252017b08ac1f4eb
wotusay <wout.salembier@hotmail.com>
2020-06-17 20:36:34 +0200

    Added boxes

--- src/components/TropicBox/tropicbox.js
+++ src/components/TropicBox/tropicbox.js
@@ -1,8 +1,11 @@
 import React from "react";
-import styles from "./adventbox.module.css"
-import { useObserver } from "mobx-react-lite";
+import styles from "./tropicbox.module.css"
 
 const TropicBox = () => {
+    return (<div className={styles.wrapper}>
+    <img className={styles.pic9}  alt='box-achterkant' src='/assets/tropic.png'  />
+</div>)
+
 
 }
 

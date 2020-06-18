2035979aa990bd7d8360a32c0632dfdcc3251ee5
wotusay <wout.salembier@hotmail.com>
2020-06-18 17:49:12 +0200

    added some css for responsive

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -17,23 +17,33 @@ const Home = () => {
             </div>
 
             <div className={styles.plane}>
-              <img
-                className={styles.planepic}
-                alt="plane"
-                src="/assets/plane.png"
-              />
+            <picture className={styles.planepic} >
+            <source media="(min-width: 1440px)" srcset="/assets/plane.png"/>
+            <source media="(min-width: 320px)" srcset="/assets/plane-mobile.png"/>
+      
+            <img  src="assets/plane.png" style={{width: 'auto'}} alt="plane"/>
+            </picture>
             </div>
           </section>
 
           <section className={styles.box}>
             <h2 className={styles.title_white}>Kies een type box</h2>
             <div className={styles.stackedbox}>
-              <img alt="stackedbox" src="/assets/stackedboxes.png" />
+              <picture>
+                <source media="(min-width: 1440px)" srcset="/assets/stackedboxes.png"/>
+                <source media="(min-width: 320px)" srcset="/assets/boxes-mobile.png"/>
+                <img alt="stackedbox" src="/assets/stackedboxes.png" style={{width: 'auto'}} />
+
+              </picture>
               <p className={styles.action}>Bekijk ons assortiment van boxen </p>
               <Link className={styles.button1}>Bekijk alle boxen</Link>
             </div>
             <div className={styles.quiz} >
-              <img alt="quizbox" src="/assets/quizbox.png" />
+              <picture>
+              <source media="(min-width: 1440px)" srcset="/assets/quizbox.png"/>
+                <source media="(min-width: 320px)" srcset="/assets/quiz-mobile.png"/>
+              <img alt="quizbox" src="/assets/quizbox.png" style={{width: 'auto'}} />
+              </picture>
               <p className={styles.action}>Laat onze test bepalen welke box het beste bij jou past </p>
               <Link className={styles.button2}>Doe de test</Link>
             </div>
@@ -61,7 +71,12 @@ const Home = () => {
                   </p>
                 </div>
                 <div className={styles.relativePos}>
-                <img className={styles.pic1} alt="mariage" src="/assets/marriage.png" /></div>
+                  <picture className={styles.pic1}>
+                  <source media="(min-width: 1440px)" srcset="/assets/marriage.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/marriage-mobile.png"/>
+                  <img style={{width: 'auto'}} alt="mariage" src="/assets/marriage.png" />
+                  </picture>
+                </div>
               </div>
             </article>
             <article>
@@ -80,35 +95,63 @@ const Home = () => {
                     herinneringen van je honeymoon at home.
                   </p>
                 </div>
-                <img className={styles.pic2} alt="openbox" src="/assets/openbox.png" />
+                <picture className={styles.pic2}>
+                <source media="(min-width: 1440px)" srcset="/assets/openbox.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/openbox-mobile.png"/>
+                <img style={{width: 'auto'}} alt="openbox" src="/assets/openbox.png" />
+                </picture>
               </div>
             </article>
             <article className={styles.gridpatern_reviewers}>
               <h3 className={styles.reviewtittle}>Wat anderen vonden </h3>
               <div className={styles.reviewers}>
                 <div className={styles.reviewpattern}>
+                  <picture>
+                  <source media="(min-width: 1440px)" srcset="/assets/reviewer-1.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/reviewer1-mobile.png"/>
                   <img alt="reviewer" src="/assets/reviewer-1.png" />
+                  </picture>
                   <p className={styles.text_rev} >
                     Geweldige ervaring gehad met mijn partner. Onze reis werd
                     uitgesteld en het was een goede vervanger!
                   </p>
-                  <img className={styles.stars} alt="stars" src="/assets/stars.png" />
+                  <picture className={styles.stars}>
+                  <source media="(min-width: 1440px)" srcset="/assets/stars.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/stars-mobile.png"/>
+                  <img style={{width: 'auto'}}  alt="stars" src="/assets/stars.png" />
+                  </picture>
                 </div>
                 <div className={styles.reviewpattern}>
-                  <img alt="reviewer" src="/assets/reviewer-2.png" />
+                <picture>
+                  <source media="(min-width: 1440px)" srcset="/assets/reviewer-2.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/reviewer2-mobile.png"/>
+                  <img style={{width: 'auto'}} alt="reviewer" src="/assets/reviewer-2.png" />
+                  </picture>
                   <p className={styles.text_rev} >
                     Geweldige ervaring gehad met mijn partner. Onze reis werd
                     uitgesteld en het was een goede vervanger!
                   </p>
-                  <img className={styles.stars} alt="stars" src="/assets/stars.png" />
+                  <picture className={styles.stars}>
+                  <source media="(min-width: 1440px)" srcset="/assets/stars.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/stars-mobile.png"/>
+                  <img style={{width: 'auto'}}  alt="stars" src="/assets/stars.png" />
+                  </picture>
                 </div>
                 <div className={styles.reviewpattern}>
-                  <img alt="reviewer" src="/assets/reviewer-3.png" />
+                <picture>
+                  <source media="(min-width: 1440px)" srcset="/assets/reviewer-3.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/reviewer3-mobile.png"/>
+                  <img style={{width: 'auto'}} alt="reviewer" src="/assets/reviewer-3.png" />
+                  </picture>
                   <p className={styles.text_rev} >
                     Geweldige ervaring gehad met mijn partner. Onze reis werd
                     uitgesteld en het was een goede vervanger!
                   </p>
-                  <img className={styles.stars} alt="stars" src="/assets/stars.png" />
+                  <picture className={styles.stars}>
+                  <source media="(min-width: 1440px)" srcset="/assets/stars.png"/>
+                  <source media="(min-width: 320px)" srcset="/assets/stars-mobile.png"/>
+                  <img style={{width: 'auto'}}  alt="stars" src="/assets/stars.png" />
+                  </picture>
                 </div>
               </div>
             </article>

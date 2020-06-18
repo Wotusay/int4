1d8f7935e3a1c956e1b054ca945933cc8460acb7
Your Name HereEwout <ewout.verhamme@student.howest.be>
2020-06-18 12:24:46 +0200

    dashboard update

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -8,7 +8,29 @@ const Dashboard = () => {
       <>
         <div className={styles.dashboard}>
           <div className={styles.content}>
-            <h1>Welkom op jullie Dashboard!</h1>
+            <div className={styles.contentIntro}>
+              <h1 className={styles.contentTitle}>
+                Welkom op jullie Dashboard!
+              </h1>
+              <p className={styles.contentText}>
+                Hier kunnen jullie doorklikken om zowel al jullie geplande
+                honeymoon-activiteiten te zien of om een kijkje te nemen in
+                jullie eigen gepersonaliseerd fotoboek.
+              </p>
+            </div>
+
+            <div className={styles.imgWrapper}>
+              <div className={styles.imgBox}>
+                <img className={styles.img} src=""></img>
+                <button className={styles.imgButton}>
+                  Jullie Activiteiten
+                </button>
+              </div>
+              <div className={styles.imgBox}>
+                <img className={styles.img}></img>
+                <button className={styles.imgButton}>Jullie Fotoboek</button>
+              </div>
+            </div>
           </div>
         </div>
       </>

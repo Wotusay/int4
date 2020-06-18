2b144f6db45088f9915e7cfe961df60dc951cee2
wotusay <wout.salembier@hotmail.com>
2020-06-18 22:00:34 +0200

    added a back button

--- src/components/Back/back.js
+++ src/components/Back/back.js
@@ -7,7 +7,9 @@ const Back = () => {
 
     return(
         <button className={styles.back__btn} onClick={() => {history.goBack()}}>
-          &larr; back
+          <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.49764 15.8986L1.09375 8.49471L8.49764 1.09082" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>  back
         </button>
       )
     

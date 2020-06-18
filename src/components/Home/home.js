908c9fdf41f087857b2851dd5dc17f605e952f61
wotusay <wout.salembier@hotmail.com>
2020-06-18 16:09:37 +0200

    added the home page for 1440px

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -25,33 +25,33 @@ const Home = () => {
             </div>
           </section>
 
-          <section>
-            <h2 className={styles.title}>Kies een type box</h2>
-            <div>
+          <section className={styles.box}>
+            <h2 className={styles.title_white}>Kies een type box</h2>
+            <div className={styles.stackedbox}>
               <img alt="stackedbox" src="/assets/stackedboxes.png" />
-              <p>Bekijk ons assortiment van boxen </p>
-              <Link>Bekijk alle boxen</Link>
+              <p className={styles.action}>Bekijk ons assortiment van boxen </p>
+              <Link className={styles.button1}>Bekijk alle boxen</Link>
             </div>
-            <div>
+            <div className={styles.quiz} >
               <img alt="quizbox" src="/assets/quizbox.png" />
-              <p>Laat onze test bepalen welke box het beste bij jou past </p>
-              <Link>Doe de test</Link>
+              <p className={styles.action}>Laat onze test bepalen welke box het beste bij jou past </p>
+              <Link className={styles.button2}>Doe de test</Link>
             </div>
           </section>
 
           <section>
-            <h2 className={styles.title}>Over ons</h2>
+            <h2 className='hidden'>Over ons</h2>
             <article>
-              <div>
-                <div>
-                  <h3>Wat is Honeyhome?</h3>
-                  <p>
+              <div className={styles.gridpatern} >
+                <div className={styles.whatWrapper}>
+                  <h3 className={styles.subtitle}>Wat is Honeyhome?</h3>
+                  <p className={styles.text} >
                     Met Honeyhome kan je je honeymoon thuis beleven. We bieden
                     verschillende soorten boxen aan die elk een verschillend
                     thema aanspreken waardoor je zelf jouw soort honeymoon kan
-                    kiezen.{" "}
+                    kiezen.
                   </p>
-                  <p>
+                  <p className={styles.text} >
                     Elke box is zorgvuldig samengesteld om je de ultieme
                     ervaring van een honeymoon thuis te geven. Er zijn
                     verschillende soorten activiteiten, van typische culturele
@@ -60,14 +60,15 @@ const Home = () => {
                     herinnering aan je honeymoon thuis.
                   </p>
                 </div>
-                <img alt="mariage" src="/assets/marriage.png" />
+                <div className={styles.relativePos}>
+                <img className={styles.pic1} alt="mariage" src="/assets/marriage.png" /></div>
               </div>
             </article>
             <article>
-              <div>
-                <div>
-                  <h3>Hoe werkt het?</h3>
-                  <p>
+              <div className={styles.gridpatern}>
+                <div className={styles.gridpos}>
+                  <h3 className={styles.subtitle}>Hoe werkt het?</h3>
+                  <p className={styles.text} >
                     Je besteld een box en krijgt deze na een paar dagen
                     opgestuurd. In de box zal een code zitten die je moet
                     ingeven op onze website bij ‘log in’. Deze code zal je
@@ -79,35 +80,35 @@ const Home = () => {
                     herinneringen van je honeymoon at home.
                   </p>
                 </div>
-                <img alt="openbox" src="/assets/openbox.png" />
+                <img className={styles.pic2} alt="openbox" src="/assets/openbox.png" />
               </div>
             </article>
-            <article>
-              <h3>Wat anderen vonden </h3>
-              <div>
-                <div>
+            <article className={styles.gridpatern_reviewers}>
+              <h3 className={styles.reviewtittle}>Wat anderen vonden </h3>
+              <div className={styles.reviewers}>
+                <div className={styles.reviewpattern}>
                   <img alt="reviewer" src="/assets/reviewer-1.png" />
-                  <p>
+                  <p className={styles.text_rev} >
                     Geweldige ervaring gehad met mijn partner. Onze reis werd
                     uitgesteld en het was een goede vervanger!
                   </p>
-                  <img alt="stars" src="/assets/stars.png" />
+                  <img className={styles.stars} alt="stars" src="/assets/stars.png" />
                 </div>
-                <div>
+                <div className={styles.reviewpattern}>
                   <img alt="reviewer" src="/assets/reviewer-2.png" />
-                  <p>
+                  <p className={styles.text_rev} >
                     Geweldige ervaring gehad met mijn partner. Onze reis werd
                     uitgesteld en het was een goede vervanger!
                   </p>
-                  <img alt="stars" src="/assets/stars.png" />
+                  <img className={styles.stars} alt="stars" src="/assets/stars.png" />
                 </div>
-                <div>
+                <div className={styles.reviewpattern}>
                   <img alt="reviewer" src="/assets/reviewer-3.png" />
-                  <p>
+                  <p className={styles.text_rev} >
                     Geweldige ervaring gehad met mijn partner. Onze reis werd
                     uitgesteld en het was een goede vervanger!
                   </p>
-                  <img alt="stars" src="/assets/stars.png" />
+                  <img className={styles.stars} alt="stars" src="/assets/stars.png" />
                 </div>
               </div>
             </article>

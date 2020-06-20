7b1a08e37644f5043a7c31f9c2623d76036749a7
wotusay <wout.salembier@hotmail.com>
2020-06-20 17:49:54 +0200

    Added html

--- src/components/Uploadpicture/uploadpicture.js
+++ src/components/Uploadpicture/uploadpicture.js
@@ -1,8 +1,47 @@
 import React from "react";
 import styles from "./uploadpicture.module.css"
+import { useObserver } from "mobx-react-lite";
+import Footer from "../Footer/footer";
+import { Link } from "react-router-dom";
+import BackBlack from "../BackBlack/backblack";
 
 const UploadPicture = () => {
-    
+    return useObserver(() => (
+        <>
+        <div className={styles.dashboard}>  
+        <BackBlack/>
+          <div className={styles.content}>
+            
+            <div className={styles.contentIntro}>
+              <p className={styles.title}>
+                Proficiat Eva & Mathias!
+              </p>
+                <p className={styles.info}>
+                Jullie zijn er in geslaagd om deze activiteit volledig uit te voeren. Hieronder kunnen jullie een foto toevoegen aan jullie fotoboek.
+                </p>
+
+                <p className={styles.subtitle}>
+                Activiteit naam - Foto uploaden
+                </p>
+
+                    <form className={styles.form}>
+                        <input className={styles.input} type="file" />
+                    </form>
+
+                <div className={styles.buttons}>
+                    <Link className={styles.button_left}>
+                    Ga terug naar de activiteiten
+                    </Link>
+                    <Link className={styles.button_right}>
+                    Volgende Stap
+                    </Link>
+                </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+        </>
+    ));
 
 }
 

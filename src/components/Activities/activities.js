1de91dd91ad85a29006bbb7f9d5b8222d1c70123
Your Name HereEwout <ewout.verhamme@student.howest.be>
2020-06-21 11:42:22 +0200

    Activities fully responsive

--- src/components/Activities/activities.js
+++ src/components/Activities/activities.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./activities.module.css";
 import { useObserver } from "mobx-react-lite";
+import Footer from "../Footer/footer";
 
 const Activities = () => {
   return useObserver(() => (
@@ -8,18 +9,48 @@ const Activities = () => {
       <div className={styles.activities}>
         <div className={styles.content}>
           <div className={styles.introWrapper}>
-            <h1 className={styles.title}>Hier zien jullie de Activiteiten!</h1>
+            <h1 className={styles.title}>
+              Hier zien jullie de <span>Activiteiten!</span>
+            </h1>
             <p className={styles.intro}>
               We hebben jullie geplande honeymoon-activiteiten onderverdeeld in
-              ochtend-, middag- en avondactiviteiten.
+              <span> ochtend-, middag- en avondactiviteiten.</span>
             </p>
           </div>
           <div className={styles.dates}>
-            <div className={styles.boxMorning}></div>
-            <div className={styles.boxNoon}></div>
-            <div className={styles.boxNight}></div>
+            <div className={styles.boxMorning}>
+              <img
+                alt="Photobook"
+                src="/assets/activities/morning_mobile.png"
+                className={styles.img}
+              ></img>
+              <button className={styles.button}>
+                Bekijk avondactiviteiten
+              </button>
+            </div>
+            <div className={styles.boxNoon}>
+              <img
+                alt="Photobook"
+                src="/assets/activities/noon_mobile.png"
+                className={styles.img}
+              ></img>
+              <button className={styles.button}>
+                Bekijk avondactiviteiten
+              </button>
+            </div>
+            <div className={styles.boxNight}>
+              <img
+                alt="Photobook"
+                src="/assets/activities/night_mobile.png"
+                className={styles.img}
+              ></img>
+              <button className={styles.button}>
+                Bekijk avondactiviteiten
+              </button>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
     </>
   ));

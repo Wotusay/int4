c75cfee3950c2a0cdda219140747024f84bb5945
Your Name HereEwout <ewout.verhamme@student.howest.be>
2020-06-21 21:26:28 +0200

    more responsive quiz

--- src/components/Quiz/quiz.js
+++ src/components/Quiz/quiz.js
@@ -11,6 +11,50 @@ const Quiz = () => {
             Klik op de foto van de gebeurtenis die je het meeste aanspreekt.
           </p>
         </div>
+
+        <div className={styles.imgWrapper}>
+          <picture className={styles.img}>
+            <source
+              media="(min-width: 1440px)"
+              srcset="/assets/quiz/tent_desktop.png"
+            />
+            <source
+              media="(min-width: 768px)"
+              srcset="/assets/quiz/tent_tablet.png"
+            />
+            <source
+              media="(min-width: 320px)"
+              srcset="/assets/quiz/tent_mobile.png"
+            />
+            <img
+              style={{ width: "auto" }}
+              src="/assets/quiz/tent_desktop.png"
+              alt="box"
+            />{" "}
+          </picture>
+          <div className={styles.vs}>
+            <p className={styles.vsText}> VS</p>
+          </div>
+          <picture className={styles.imgRight}>
+            <source
+              media="(min-width: 1440px)"
+              srcset="/assets/quiz/bath_desktop.png"
+            />
+            <source
+              media="(min-width: 768px)"
+              srcset="/assets/quiz/bath_tablet.png"
+            />
+            <source
+              media="(min-width: 320px)"
+              srcset="/assets/quiz/bath_mobile.png"
+            />
+            <img
+              style={{ width: "auto" }}
+              src="/assets/quiz/bath_desktop.png"
+              alt="box"
+            />{" "}
+          </picture>
+        </div>
       </div>
     </>
   );

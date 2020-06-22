af8f95b358aa0fe454b0353e87d06ec8afb21bdb
wotusay <wout.salembier@hotmail.com>
2020-06-22 10:30:31 +0200

    Added desktop mode for activities page

--- src/components/Activities/activities.js
+++ src/components/Activities/activities.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./activities.module.css";
 import { useObserver } from "mobx-react-lite";
 import Footer from "../Footer/footer";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../consts";
 
 const Activities = () => {
   return useObserver(() => (
@@ -19,34 +21,50 @@ const Activities = () => {
           </div>
           <div className={styles.dates}>
             <div className={styles.boxMorning}>
-              <img
-                alt="Photobook"
-                src="/assets/activities/morning_mobile.png"
-                className={styles.img}
-              ></img>
-              <button className={styles.button}>
-                Bekijk avondactiviteiten
-              </button>
+              <picture className={styles.img}>
+              <source media="(min-width: 1440px)" srcset="/assets/activities/morning.png"/>
+                <source media="(min-width: 320px)" srcset="/assets/activities/morning_mobile.png"/>
+                       <img
+                alt="morning"
+                style={{width:"auto"}}
+                src="/assets/activities/morning.png"
+              />
+              </picture>
+       
+              <Link to={ROUTES.morningRoom} className={styles.button}>
+                Bekijk ochtendactiviteiten
+              </Link>
             </div>
             <div className={styles.boxNoon}>
-              <img
-                alt="Photobook"
-                src="/assets/activities/noon_mobile.png"
-                className={styles.img}
-              ></img>
-              <button className={styles.button}>
-                Bekijk avondactiviteiten
-              </button>
+              <picture className={styles.img}>
+              <source media="(min-width: 1440px)" srcset="/assets/activities/noon.png"/>
+                <source media="(min-width: 320px)" srcset="/assets/activities/noon_mobile.png"/>
+                     <img
+                alt="noon"
+                style={{width:"auto"}}
+                src="/assets/activities/noon.png"
+              />
+              </picture>
+         
+              <Link to={ROUTES.afternoonRoom}  className={styles.button}>
+                Bekijk middagactiviteiten
+              </Link>
             </div>
             <div className={styles.boxNight}>
+              <picture className={styles.img}>
+              <source media="(min-width: 1440px)" srcset="/assets/activities/night.png"/>
+                <source media="(min-width: 320px)" srcset="/assets/activities/night_mobile.png"/>
               <img
-                alt="Photobook"
-                src="/assets/activities/night_mobile.png"
-                className={styles.img}
-              ></img>
-              <button className={styles.button}>
+                alt="night"
+                style={{width:"auto"}}
+                src="/assets/activities/night.png"
+
+              />
+              </picture>
+            
+              <Link to={ROUTES.nightRoom} className={styles.button}>
                 Bekijk avondactiviteiten
-              </button>
+              </Link>
             </div>
           </div>
         </div>

b7d5bbcf43e1d4b9305cc0ffb9868de936f953e1
Your Name HereEwout <ewout.verhamme@student.howest.be>
2020-06-23 16:54:03 +0200

    Yoga page fully responsive and working fine

--- src/components/YogaDetail/yogaDetail.js
+++ src/components/YogaDetail/yogaDetail.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import styles from "./yogaDetail.module.css";
 import Back from "../BackBlack/backblack";
+import { ROUTES } from "../../consts";
+import { useHistory } from "react-router-dom";
 
 const YogaDetail = () => {
   let [number = 1, setNumber] = useState(1);
   let title = "Twin Trees (Beginner)";
   let img = `/assets/yoga/pose1.png`;
+  let imgMobile = `/assets/yoga/pose1_mobile.png`;
+  const history = useHistory();
 
   switch (number) {
     case (number = 0):
@@ -13,38 +17,43 @@ const YogaDetail = () => {
       break;
 
     case (number = 1):
+      imgMobile = "/assets/yoga/pose1_mobile.png";
       img = "/assets/yoga/pose1.png";
       title = "Twin Trees (Beginner)";
       break;
 
     case (number = 2):
+      imgMobile = "/assets/yoga/pose2_mobile.png";
       img = "/assets/yoga/pose2.png";
       title = "Chair Pose (Beginner)";
       break;
 
     case (number = 3):
+      imgMobile = "/assets/yoga/pose3_mobile.png";
       img = "/assets/yoga/pose3.png";
       title = "Buddy Boat (Intermediate)";
       break;
 
     case (number = 4):
+      imgMobile = "/assets/yoga/pose4_mobile.png";
       img = "/assets/yoga/pose4.png";
       title = "Square Pose (Intermediate)";
       break;
 
     case (number = 5):
+      imgMobile = "/assets/yoga/pose5_mobile.png";
       img = "/assets/yoga/pose5.png";
       title = "Plank on plank (Advanced)";
       break;
 
     case (number = 6):
+      imgMobile = "/assets/yoga/pose6_mobile.png";
       img = "/assets/yoga/pose6.png";
       title = "Square Pose (Intermediate)";
       break;
 
-    case (number = 6):
-      img = "/assets/yoga/pose6.png";
-      title = "Square Pose (Intermediate)";
+    case (number = 7):
+      history.push(ROUTES.uploadpicture);
       break;
 
     default:
@@ -60,7 +69,12 @@ const YogaDetail = () => {
         </div>
         <div className={styles.content}>
           <h1 className={styles.title}>Doe de yoga poses na!</h1>
-          <img className={styles.img} alt="Yoga Pose" src={img} />
+          <picture className={styles.img}>
+            <source media="(min-width: 1440px)" srcSet={img} />
+            <source media="(min-width: 320px)" srcSet={imgMobile} />
+            <img alt="Yoga Pose" style={{ width: "auto" }} src={img} />
+          </picture>
+          <p className={styles.yogaTitle}>{title}</p>
           <div className={styles.buttonWrapper}>
             <button
               onClick={() => setNumber(number - 1)}
@@ -68,7 +82,7 @@ const YogaDetail = () => {
             >
               Vorige pose
             </button>
-            <p className={styles.yogaTitle}>{title}</p>
+
             <button
               onClick={() => setNumber(number + 1)}
               className={styles.button}
